603623232f7739321f2c070dadd4ef572c8a5473
Steven Reale <steven.reale@mayvue.com>
2025-08-12 14:58:04 -0400

    chore: repo and tooling created

---
+++ apps/web/src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>Counterpoint Linter</h1>
+      <p>It works! í¾¶</p>
+    </div>
+  );
+}
---
+++ packages/engine/src/index.ts
@@ -0,0 +1,2 @@
+export const version = "0.0.0";
+/** TODO: export Rule, Diagnostic, parseScore, run(), etc. in Milestone 1 */
