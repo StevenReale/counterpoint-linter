750b709b9caf11526b5220eeb23f94a22c3b810b
Steven Reale <steven.reale@mayvue.com>
2025-08-12 15:34:08 -0400

    chore: cleanup repo and tooling

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -2,6 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+console.log("main.tsx loaded"); // quick sanity check
+
+const rootEl = document.getElementById("root");
+if (!rootEl) throw new Error("Root element not found");
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
